b29c558975aab0a653618d513bf6621dd46d7386
stathis7888 <49855695+stathis7888@users.noreply.github.com>
2020-09-24 14:51:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.get('/', (req, res) => {
 app.get('/download', (req,res) => {
   var URL = req.query.URL;
   var type = req.query.type;
+  console.log(type)
   console.log(URL)
   res.header('Content-Disposition', 'attachment; filename="video.mp3"');
     /*ytdl(URL, {
