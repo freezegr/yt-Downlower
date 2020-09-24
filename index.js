c23783c08e46a14601df3f33a349b3fc81e8d416
freezegr <statsaras2006@gmail.com>
2020-09-24 12:02:45 +0300

    update

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 
-const app = express();app.use(cors());app.listen(4000, () => {
+const app = express();
+app.use(cors());
+
+app.set('view engine', 'ejs')
+app.listen(4000, () => {
   console.log('Server Works !!! At port 4000');
 });
+
+app.get('/', (req, res) => { 
+  res.render('index')
+})
+
 app.get('/download', (req,res) => {
-  var URL = req.query.URL;res.header('Content-Disposition', 'attachment; filename="video.mp3"');
+  var URL = req.query.URL;
+  console.log(URL)
+  res.header('Content-Disposition', 'attachment; filename="video.mp3"');
     ytdl(URL, {
       format: 'mp3'
     }).pipe(res);
