575265782b6d2c75f40b2a952ae437fe1f6c126d
freezegr <49855695+freezegr@users.noreply.github.com>
2020-09-26 22:57:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.tag('/test', (req, res) => {
+  res.render('play');
+
+});
+
 app.get('/download', async (req,res) => {
   var URL = req.query.URL;
   var type = req.query.type;
@@ -55,6 +60,57 @@ app.get('/download', async (req,res) => {
     }).pipe(res)
   }
 });
+
+app.get('/play', async (req,res) => {
+  var URL = req.query.URL;
+  var type = req.query.type;
+  if(URL == undefined) return res.send('<script>alert("The url is not true")</script>')
+  if(type == undefined) return res.redirect('https://yt-music-installer.herokuapp.com/?error=noType');
+  if(type != 'mp3' && type != 'mp4') return res.redirect('https://yt-music-installer.herokuapp.com/?error=falseType');
+  console.log(type)
+  console.log(URL)
+  function playSound(url) {
+    var a = new Audio(url);
+    a.play()
+}
+  function (){
+    try {
+     playSound(URL)
+    }catch(err) {
+      console.log(err)
+    }
+  }
+
+  /*async function info(){
+    try {
+      return await ytdl.getInfo(URL);
+    }catch(err) {
+      return res.redirect('https://yt-music-installer.herokuapp.com/?error=falseVideo');
+    }
+  } 
+  let inform = await info()
+  if(inform.title == undefined) return;
+  //console.log(inform.title)
+  if(type == 'mp3'){
+    let mp3title = `${inform.title}.mp3`
+    console.log(mp3title)
+    res.header('Content-Disposition', contentDisposition(mp3title));  
+    ytdl(URL, {
+      format: 'mp3',
+      quality: 'highestaudio',
+      filter: 'audioonly'
+    }).pipe(res);
+
+  }else {
+    let mp4Title = `${inform.title}.mp4`
+    res.header('Content-Disposition', contentDisposition(mp4Title));
+    ytdl(URL, {
+      form: 'mp4',
+      quality: 'highestvideo'
+    }).pipe(res)
+  }*/
+});
+
 
 app.use(function(req, res){
   res.status(404)
